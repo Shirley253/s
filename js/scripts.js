4f9a3af96dbc2f993723b209797fa194a6d7c0fd
andrew lupton and shirley dennis <shirley@>
2018-03-20 11:22:36 -0700

    add sandcastles project

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+  $(".clickable").click(function() {
+    $("#sandcastle1-showing").slideToggle();
+    $("#sandcastle1-hidden").slideToggle();
+  });
+});
+$(function(){
+  $(".clickable").click(function() {
+    $("#sandcastle2-showing").slideToggle();
+    $("#sandcastle2-hidden").slideToggle();
+  });
+});
+$(function(){
+  $(".clickable").click(function() {
+    $("#sandcastle3-showing").slideToggle();
+    $("#sandcastle3-hidden").slideToggle();
+  });
+});
